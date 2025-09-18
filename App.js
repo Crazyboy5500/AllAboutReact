b72c64080c6ccd9718ca2570355018976a3e7a0d
shaan <adityakm5500@gmail.com>
2025-09-18 22:35:39 +0530

    Day 2 of learning react

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", {id:"parent"},
     [
         React.createElement("div",{id:"child1"},
-            [React.createElement("h1",{},"I am a h1 tag of child1") , React.createElement("h2",{},"I am a h2 tag of child1")]
+            [React.createElement("h1",{},"updation on child1 in an instant displayed") , React.createElement("h2",{},"I am a h2 tag of child1")]
         ),
         React.createElement("div",{id:"child2"},
             [React.createElement("h1",{},"I am a h1 tag of child2") , React.createElement("h2",{},"I am a h2 tag of child2")]
